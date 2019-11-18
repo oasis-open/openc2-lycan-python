a2acc671cae4ee5cd2242de7d7fdd431d2f5566e
Stair, Michael (ms1784) <ms1784@us.att.com>
2019-11-18 11:15:50 -0500

    Refactor using stix2-python for update to 1.0 spec

--- openc2/v10/common.py
+++ openc2/v10/common.py
@@ -20,32 +20,26 @@
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-import os
-from distutils.core import setup
-from setuptools import find_packages
+"""
+.. module: openc2.v10.common
+    :platform: Unix
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-VERSION_FILE = os.path.join(BASE_DIR, 'openc2', 'version.py')
+.. version:: $$VERSION$$
+.. moduleauthor:: Michael Stair <mstair@att.com>
 
-def get_version():
-    with open(VERSION_FILE) as f:
-        for line in f.readlines():
-            if line.startswith('__version__'):
-                version = line.split()[-1].strip('"')
-                return version
-        raise AttributeError("Package does not have a __version__")
+"""
 
-setup(
-    name='openc2',
-    version=get_version(),
-    description='Produce and consume OpenC2 JSON messages',
-    packages=find_packages(exclude=["tests"]),
-    license='MIT',
-    include_package_data=True,
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    install_requires=[
-        'six',
-        'stix2'
-    ],
-)
+from stix2 import properties
+from collections import OrderedDict
+from ..base import _OpenC2DataType
+
+class Payload(_OpenC2DataType):
+    _type = 'payload'
+    _properties = OrderedDict([
+        ('bin', properties.BinaryProperty()),
+        ('url', properties.StringProperty())
+    ])
+
+    def _check_object_constraints(self):
+        super(Payload, self)._check_object_constraints()
+        self._check_mutually_exclusive_properties(['bin', 'url'])
---
+++ openc2/v10/slpf.py
@@ -0,0 +1,107 @@
+#
+#  The MIT License (MIT)
+#
+# Copyright 2019 AT&T Intellectual Property. All other rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+# and associated documentation files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+# AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+"""
+.. module: openc2.v10.slpf
+    :platform: Unix
+
+.. version:: $$VERSION$$
+.. moduleauthor:: Michael Stair <mstair@att.com>
+
+"""
+
+from stix2 import properties
+from ..base import _OpenC2Base, _Actuator, _Target
+from ..custom import _custom_actuator_builder
+from ..properties import TargetProperty, ActuatorProperty, ArgsProperty
+
+import itertools
+from collections import OrderedDict
+
+class SLPFActuator(_Actuator):
+    _type = 'slpf'
+    _properties = OrderedDict([
+        ('hostname', properties.StringProperty()),
+        ('named_group', properties.StringProperty()),
+        ('asset_id', properties.StringProperty()),
+        ('asset_tuple', properties.StringProperty()),
+    ])
+
+class SLPFTarget(_Target):
+    _type = 'slpf:rule_number'
+    _properties = OrderedDict([
+        ('slpf:rule_number', properties.IntegerProperty(required=True)),
+    ])
+
+class SLPFArgs(_OpenC2Base):
+    _type = 'slpf'
+    _properties = OrderedDict([
+        ('drop_process', properties.EnumProperty(
+            allowed=[
+                "none",
+                "reject",
+                "false_ack",
+            ] 
+        )),
+        ('persistent', properties.EnumProperty(
+            allowed=[
+                "none",
+                "reject",
+                "false_ack",
+            ] 
+        )),
+        ('direction', properties.EnumProperty(
+            allowed=[
+                "both",
+                "ingress",
+                "egress",
+            ] 
+        )),
+        ('insert_rule', properties.IntegerProperty()),
+    ])
+
+class SLPF(_OpenC2Base):
+    _type = 'slpf'
+    _properties = OrderedDict([
+        ('action', properties.EnumProperty(
+            allowed=[
+                "query",
+                "deny",
+                "allow",
+                "update",
+                "delete",
+            ], required=True
+        )),
+        ('target', TargetProperty(required=True)),
+        ('args', ArgsProperty()),
+        ('actuator', ActuatorProperty()),
+        ('command_id', properties.StringProperty())
+    ])
+
+    def _check_object_constraints(self):
+        super(SLPF, self)._check_object_constraints()
+        if not isinstance(self.target, _Target) or not self.target.type \
+                in ['features', 'file', 'ipv4_net', 'ipv6_net', 'ipv4_connection', 
+                        'ipv6_connection', 'slpf:rule_number']:
+            raise ValueError("Unsupported target (%s)"%self.target)
+        if self.actuator and not isinstance(self.actuator, SLPFActuator):
+            raise ValueError("Unsupported actuator (%s)"%self.actuator._type)
