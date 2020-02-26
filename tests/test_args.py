447a73c11c791a39289ecd3f44bffd9b7591697b
twodayslate <zac@gorak.us>
2020-02-26 09:34:22 -0500

    Improved test cases

--- tests/test_args.py
+++ tests/test_args.py
@@ -1,10 +1,11 @@
 import openc2
 import pytest
+import json
 
 import stix2.exceptions
 
 
-def test_args_simple():
+def test_args_response_requested():
     valid = ["none",
                 "ack",
                 "status",
@@ -12,6 +13,10 @@ def test_args_simple():
     for v in valid:
         a = openc2.v10.Args(response_requested=v)
         print(a)
+        ser_a = json.loads(a.serialize())
+        b = openc2.v10.Args(**ser_a)
+        assert a == b
+
 
     with pytest.raises(stix2.exceptions.InvalidValueError):
         openc2.v10.Args(response_requested="bad")
--- tests/test_targets.py
+++ tests/test_targets.py
@@ -1,6 +1,6 @@
 import openc2
 import pytest
-
+import json
 import stix2.exceptions
 
 
@@ -9,6 +9,10 @@ def test_ipv4_address_example():
         ipv4_net="198.51.100.3")
     
     assert ip4.ipv4_net == "198.51.100.3"
+
+    ser_ipv4 = json.loads(ip4.serialize())
+    ip4_2 = openc2.v10.IPv4Address(**ser_ipv4)
+    assert ip4 == ip4_2
 
     ip4 = openc2.v10.IPv4Address(
         ipv4_net="198.51.100.0/24")
