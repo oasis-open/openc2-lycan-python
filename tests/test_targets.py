dbf2f0ada8f32bd79ccfeb6b4657c352055a2d40
twodayslate <zac@gorak.us>
2020-03-03 13:54:46 -0500

    Added additional test case and fixes for properties inside lists

--- tests/test_targets.py
+++ tests/test_targets.py
@@ -45,6 +45,48 @@ def test_custom_target():
         openc2.parse(one.serialize())
 
 
+def test_multiple_custom_targets():
+    @openc2.CustomTarget("x-thing:id", [("id", stix2.properties.StringProperty())])
+    class CustomTarget(object):
+        pass
+
+    @openc2.CustomTarget("x-thing:name", [("name", stix2.properties.StringProperty())])
+    class CustomTarget2(object):
+        pass
+
+    one = CustomTarget()
+    assert one != None  # for some reason `assert one` fails
+
+    with pytest.raises(stix2.exceptions.ExtraPropertiesError):
+        CustomTarget(bad="id")
+
+    one = CustomTarget(id="uuid")
+    assert one
+    assert one.id == "uuid"
+
+    two = CustomTarget(id=(json.loads(one.serialize())["x-thing:id"]))
+    assert one == two
+
+    with pytest.raises(stix2.exceptions.ParseError):
+        openc2.parse(one.serialize())
+
+    foo = CustomTarget2()
+    assert foo != None
+
+    with pytest.raises(stix2.exceptions.ExtraPropertiesError):
+        CustomTarget2(bad="id")
+
+    one = CustomTarget2(name="name")
+    assert one
+    assert one.name == "name"
+
+    two = CustomTarget2(name=(json.loads(one.serialize())["x-thing:name"]))
+    assert one == two
+
+    with pytest.raises(stix2.exceptions.ParseError):
+        openc2.parse(one.serialize())
+
+
 def test_custom_target_required():
     @openc2.CustomTarget(
         "x-thing:id", [("id", stix2.properties.StringProperty(required=True))]
@@ -81,7 +123,9 @@ def test_custom_target_with_custom_property():
     class CustomTargetProperty(object):
         pass
 
-    @openc2.CustomTarget("x-thing:id", [("id", CustomTargetProperty(required=True))])
+    @openc2.CustomTarget(
+        "x-thing:id", [("id", CustomTargetProperty(required=True, default=lambda: {}))]
+    )
     class CustomTarget(object):
         pass
 
@@ -103,6 +147,7 @@ def test_custom_target_with_custom_property():
     # property with one value
 
     one = CustomTarget(id=CustomTargetProperty(name="name"))
+    assert one != None
     assert one.id.name == "name"
 
     two = CustomTarget(id=(json.loads(one.serialize())["x-thing:id"]))
@@ -122,3 +167,23 @@ def test_custom_target_with_custom_property():
 
     two = CustomTarget(id=(json.loads(one.serialize())["x-thing:id"]))
     assert one == two
+
+    @openc2.CustomTarget(
+        "x-thing:list",
+        [("list", openc2.properties.EmptyListProperty(CustomTargetProperty))],
+    )
+    class CustomTarget2(object):
+        pass
+
+    foo = CustomTarget2()
+    assert foo != None
+
+    foo = CustomTarget2(
+        list=[CustomTargetProperty(name="name", uid="uid", version="version")]
+    )
+    assert foo != None
+    assert foo.list != None
+    assert foo.list[0] != None
+    assert foo.list[0].name == "name"
+    assert foo.list[0].uid == "uid"
+    assert foo.list[0].version == "version"
